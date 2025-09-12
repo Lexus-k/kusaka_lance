6ce9bfc36eeee8ea48ab907df7350658f49f3893
kusaka_lance <kchrstophr@gmail.com>
2025-09-12 22:36:34 +0800

    Configured database connection with env variables

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lance/kusaka_lance',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'db882151e135c32ba0c881ad344fc77874857ea4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lance/kusaka_lance' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'db882151e135c32ba0c881ad344fc77874857ea4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
