dd85d9f249b48bee551ed8c83d8b6f28457a4aa7
kusaka_lance <kchrstophr@gmail.com>
2025-09-25 00:35:30 +0800

    config path

--- app/config/config.php
+++ app/config/config.php
@@ -90,7 +90,7 @@ $config['base_url'] 				= 'https://kusaka-lance.onrender.com';
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '.htaccess';
 
 /*
 |--------------------------------------------------------------------------
